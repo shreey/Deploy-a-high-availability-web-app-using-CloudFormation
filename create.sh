7200ecf7c0ba4be8e8cba4827d39cd6f84217577
shreey <shreef7z7elkady@gmail.com>
2022-09-08 01:53:15 +0200

    This is a new commit for what I originally planned to be amended

---
+++ create.sh
@@ -0,0 +1,2 @@
+aws cloudformation create-stack --stack-name dummy-application --template-body file://dummy-application.yml  --parameters file://dummy-application.json --region=us-east-1 --capabilities CAPABILITY_NAMED_IAM
+
---
+++ update.sh
@@ -0,0 +1,2 @@
+aws cloudformation update-stack --stack-name dummy-application --template-body file://dummy-application.yml  --parameters file://dummy-application.json --region=us-east-1 --capabilities CAPABILITY_NAMED_IAM
+
